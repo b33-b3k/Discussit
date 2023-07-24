9b536ed3f99fc1e7f8fc618e13a475a82de97ba5
Bibek <ba03021321@ku.edu.np>
2023-07-24 19:50:49 +0545

    next:modtools

--- discussit/lib/features/community/screens/community_screen.dart
+++ discussit/lib/features/community/screens/community_screen.dart
@@ -1,4 +1,5 @@
 import 'package:discussit/core/common/error_text.dart';
+import 'package:discussit/features/auth/controller/auth_controller.dart';
 import 'package:discussit/features/auth/screen/loginScreen.dart';
 import 'package:discussit/features/community/repository/community_repo.dart';
 import 'package:flutter/material.dart';
@@ -12,6 +13,8 @@ class CommunityScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final user = ref.watch(userProvider);
+
     return Scaffold(
         body: ref.watch(getCommunitybyNameProvider(name)).when(
             data: (community) => NestedScrollView(
@@ -22,9 +25,72 @@ class CommunityScreen extends ConsumerWidget {
                       floating: true,
                       expandedHeight: 150,
                       flexibleSpace: Stack(children: [
-                        Positioned.fill(child: Image.network(community.banner))
+                        Positioned.fill(
+                            child: Image.network(community.banner,
+                                fit: BoxFit.cover))
                       ]),
                       snap: true,
+                    ),
+                    SliverPadding(
+                      padding: const EdgeInsets.all(16),
+                      sliver: SliverList(
+                          delegate: SliverChildListDelegate([
+                        Align(
+                          alignment: Alignment.topLeft,
+                          child: CircleAvatar(
+                            radius: 36,
+                            backgroundImage: NetworkImage(
+                              community.avatar,
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          height: 4,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "d/${community.name}",
+                              style: TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                            community.moderators.contains(user?.uid)
+                                ? OutlinedButton(
+                                    onPressed: () {},
+                                    style: ElevatedButton.styleFrom(
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 25),
+                                    ),
+                                    child: const Text(" Settings "))
+                                : OutlinedButton(
+                                    onPressed: () {},
+                                    style: ElevatedButton.styleFrom(
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                      padding: const EdgeInsets.symmetric(
+                                          horizontal: 25),
+                                    ),
+                                    child: Text(
+                                        community.members.contains(user?.uid)
+                                            ? "Joined"
+                                            : " Join "),
+                                  )
+                          ],
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 0),
+                          child: Text(
+                            "${community.members.length} members",
+                            style: TextStyle(
+                                fontSize: 12, fontWeight: FontWeight.w100),
+                          ),
+                        ),
+                      ])),
                     )
                   ];
                 }),
